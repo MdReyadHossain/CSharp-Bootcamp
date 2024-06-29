ca6a1724ed088cdedc4c0a7a12ec6b96a056919e
MdReyadHossain <reyadhosen@gmail.com>
2024-06-29 18:33:36 +0600

    array

---
+++ Array/Program.cs
@@ -0,0 +1,60 @@
+﻿/** single-dimentional Array **/
+
+int[] arrInit1 = new int[2] { 10, 20 };
+
+int[] arr1 = new int[3]; // set an array with size 3
+arr1[0] = 1;
+arr1[1] = 2;
+arr1[2] = 3;
+foreach (int item in arr1)
+{
+    Console.Write(item + " ");
+}
+Console.WriteLine();
+Console.WriteLine("Its {0} dimentional array", arr1.Rank); // define the dimention: 1
+
+
+
+/** multi-dimentional array **/
+// [row, column]
+// should be same numbers of column of every rows
+int[,] arrInit2 = new int[2, 2]
+{
+                {10, 20 },
+                {30, 40 }
+};
+
+int[,] arr2 = new int[2, 3];    // set an array with size 2 x 3
+arr2[0, 0] = 1; arr2[0, 1] = 2; arr2[0, 2] = 3; // [[1, 2, 3],
+arr2[1, 0] = 4; arr2[1, 1] = 5; arr2[1, 2] = 6; //  [4, 5, 6]]
+
+// travarse the array
+for (int i = 0; i < 2; i++) // row
+{
+    for (int j = 0; j < 3; j++) // column
+    {
+        Console.Write(arr2[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+
+// set array with 2 x 2 x 3                                 // [
+int[,,] arr3 = new int[2, 2, 3];                            //     [[1, 2, 3],        
+arr3[0, 0, 0] = 1; arr3[0, 0, 1] = 2; arr3[0, 0, 2] = 3;    //      [4, 5, 6]],
+arr3[0, 1, 0] = 4; arr3[0, 1, 1] = 5; arr3[0, 1, 2] = 6;    //     [[40, 50, 60]
+arr3[1, 0, 0] = 10; arr3[1, 0, 1] = 20; arr3[1, 0, 2] = 30; //      [10, 20, 30]],
+arr3[1, 1, 0] = 40; arr3[1, 1, 1] = 50; arr3[1, 1, 2] = 60; // ]
+
+
+
+
+/** Jacked Array **/
+// can be different numbers of column
+int[][] arr4 = new int[3][];
+arr4[0] = new int[4] { 1, 3, 5, 7 };
+arr4[1] = new int[3] { 10, 30, 50 };
+arr4[2] = new int[5] { 0, 2, 4, 6, 8 };
+
+// access an element
+arr4[1][2] = 70;
+Console.WriteLine($"arr4[1][2]: {arr4[1][2]}");
---
+++ String/Program.cs
@@ -0,0 +1,27 @@
+﻿string name = "Reyad Hossain";
+string[] names = name.Split(',', ' '); // returs an array of string by split ["Reyad", "Hossain"]
+string femaleName = name.Substring(0, 4); // Reya, name.Substring(8): ssain
+Console.WriteLine($"First Name is {names[0]} and Last name is {names[1]}\n");
+Console.WriteLine("femaleName = {0}\n", femaleName);
+
+int ascii = 'R';
+Console.WriteLine("Ascii value of R is {0}\n", ascii);
+
+bool isStartWith = name.StartsWith('R');
+Console.WriteLine($"Is {names[0]} start with R? {isStartWith}\n");
+
+uint bin = 0b_0000_1111_0000_1111_0000_1111_0000_1010;
+uint bin1sComplement = ~bin << 4;
+Console.WriteLine($"{Convert.ToString(bin, toBase: 2)}\n{Convert.ToString(bin1sComplement, toBase: 2)}\n");
+
+uint bin1 = 0b_1010_0111;
+uint bin2 = 0b_0110_0101;
+uint andOp = bin1 & bin2;
+uint orOp = bin1 | bin2;
+uint xorOp = bin1 ^ bin2;
+
+Console.WriteLine(  $"And = {Convert.ToString(andOp, toBase: 2)}" +
+                    $"\nOr = {Convert.ToString(orOp, toBase: 2)}" +
+                    $"\nXor = {Convert.ToString(xorOp, toBase: 2)}\n");
+
+Console.WriteLine($"3rd letter of name: {name[2]}\n"); // can not assign a value to the index of this name
